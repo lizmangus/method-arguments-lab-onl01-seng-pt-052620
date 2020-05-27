168c6931b048dba51da206f325974d7203dc8056
Elizabeth Mangus <lizmangus@ymail.com>
2020-05-27 14:39:34 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,7 +13,7 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language(name, language)
+def introduction(name, language)
   puts "Hi, my name is #{name} and I am learning #{language}"
 end
 
